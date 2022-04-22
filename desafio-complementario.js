64908cde77feb047ac675da6acd9f6e064c7c6b8
Gisi.Peru <gisibodnar@gmail.com>
2022-04-21 23:32:09 -0300

    prueba

---
+++ desafio-complementario.js
@@ -0,0 +1,5 @@
+let resultadosTest = 0;
+let promedio = 0;
+for(let i = 0; i < resultadosTest.length; i++) {
+    promedio = promedio + resultadosTest[i]
+} console.log( promedio / resultadosTest.length)
